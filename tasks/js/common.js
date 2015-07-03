3627f48edd6daf478b77f23d5536d0092dbdcaa0
Joseph Topasna <josephtopasna@D-69-91-163-149.dhcp4.washington.edu>
2015-07-02 18:02:23 -0700

    modified all js

--- tasks/js/common.js
+++ tasks/js/common.js
@@ -1,7 +1,8 @@
-"use strict";
+// Recommended: put "use strict" in js
+"use strict"; // changes interepreter into a strict mode. Flags errors for you.
 
 //initialize Parse library with your application ID and your app's JavaScript library key
-Parse.initialize('your application id', 'your app JavaScript library key');
+Parse.initialize('93eXNi1LeHr149KRNYlRaowQC24f2SftlpXwYyU8', 'unXjVtpunbxHYqZIhXw6q598aVIXg4su9xY4D2Nv');
 
 /**
  * Shows an error in an element on the page with the class 'error-message'
--- tasks/js/tasks.js
+++ tasks/js/tasks.js
@@ -8,3 +8,64 @@
  */
 
 //use jQuery to register a function that is called when the document is ready for manipulation
+
+// Reroutes to sign in page if user is not logged in
+$(function() {
+	var currentUser = Parse.User.current();
+	if (!currentUser) {
+		window.location = 'signin.html';
+	}
+
+	$('.nav-link-sign-out').click(function(evt) {
+		evt.preventDefault();
+		Parse.User.logOut();
+		window.location = 'signin.html';
+	});
+
+	$('.user-name').text(currentUser.get('firstname') + ' ' + currentUser.get('lastname'));
+
+	// Upper case for class/tasks
+	var Task = Parse.Object.extend('Task');
+
+	var tasksQuery = new Parse.Query(Task);
+	tasksQuery.equalTo('user', currentUser); 
+	tasksQuery.ascending('done, createdAt'); // one string only. this is correct.
+
+	var TaskList = Parse.Collection.extend({ //curly braces create a javascript object on the fly
+		model: Task,
+		query: tasksQuery
+	});
+
+	var tasks = new TaskList();
+
+	tasks.on('all', function() {
+		var taskList = $('.task-list');
+		taskList.empty();
+
+		this.forEach(function(task) {
+			var taskItem = $(document.createElement('li'));
+			taskItem.text(task.get('title'));
+			taskList.append(taskItem)
+		});
+	});
+
+	tasks.fetch();
+
+	$('.form-new-task').submit(function(evt) {
+		evt.preventDefault();
+		var newTaskForm = $(this); // so you can use jquery on this element
+		var newTitleInput = newTaskForm.find('.new-task-title');
+		var newTask = new Task();
+		newTask.set('title', newTitleInput.val());
+		newTask.set('user', currentUser);
+		newTask.set('done', false);
+
+		newTask.save().then(function() {
+			tasks.add(newTask);
+			newTitleInput.val(''); // to clear an input 
+		}, function(err) {
+			showError(err);
+		});
+	});
+
+});
